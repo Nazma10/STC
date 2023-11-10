eb62f4c1a72ca0bd4085d70d6b21f8e33dcba3e5
Nazma10 <nazmanazar5@gmail.com>
2023-11-10 09:10:10 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from './Components/Login';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Login/>
+      <Footer/>
     </div>
   );
 }
